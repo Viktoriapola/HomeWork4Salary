f75ee9d165c42b0b249d9a9f2df2f3100633f9e2
Viktoriapola <viktoriapola8@gmail.com>
2021-03-26 00:29:04 +0300

    add 2 file

---
+++ script.js
@@ -0,0 +1,49 @@
+// в этом способе я не смогда добавить то, что бы пользователь видел что он вводит что о не то
+
+function sumSalary() {
+
+    let numbers = [];
+   
+    while (true) {
+  
+      let getSalary = prompt('Введите зарплату за месяц');
+  
+      if (getSalary === '' || getSalary === null) {
+        break;
+      } 
+      numbers.push(+getSalary);
+    }
+  
+    let sum = 0;
+    for (let num of numbers) {
+      sum += num / numbers.length;
+    }
+    return sum;
+}
+  
+alert('Средняя зарплата ' + sumSalary() );
+
+
+
+
+
+//это второй способ
+
+
+// let summa = 0;
+// let getSalary;
+// let div = -1;
+ 
+// do {
+//   getSalary = +prompt('Введите зарплату за месяц');
+ 
+//   if (isFinite(getSalary)) {
+//     div++;
+//     summa += getSalary;
+//   } else {
+//     alert('Было введено не число, попробуйте еще раз');
+//   }
+// } while (getSalary !== 0);
+// alert(`Средняя зарплата ${summa / div}`);
+
+
