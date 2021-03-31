dcdee32bda64673ab0a969a17392975561fcbf3e
Viktoriapola <viktoriapola8@gmail.com>
2021-03-31 21:31:36 +0300

    add changes

--- script.js
+++ script.js
@@ -1,27 +1,67 @@
+
+let sum = 0;
+let average = 0;
+let history = "";
+let numberOfSalaries = 0;
+
+while (true) {
+  let salary = prompt(getInfo(sum, average, history) + '\nВведите ЗП' );
+  if (salary === null) {
+    if (confirm('Хотите выйти?')) {
+      break;
+    }
+  }
+
+  if (isNaN(Number(salary))) {
+    if (confirm('Выдопустили ошибку! Хотите выйти?')) {
+      break;
+    }
+    continue;
+  }
+
+  numberOfSalaries++;
+  salary = Number(salary);
+  sum += salary;
+  average = sum / numberOfSalaries;
+  history += salary + 'руб. ';
+};
+
+alert(getInfo(sum, average, history));
+
+function getInfo(sum, average, history) {
+  return `Сумма: ${sum}; \nСредняя: ${average}; \nИстория: ${history}`;
+};
+
+
+
+
+
+
+
 // в этом способе я не смогда добавить то, что бы пользователь видел что он вводит что о не то
 
-function sumSalary() {
+// function sumSalary() {
 
-    let numbers = [];
+//     let numbers = [];
    
-    while (true) {
+//     while (true) {
   
-      let getSalary = prompt('Введите зарплату за месяц');
+//       let getSalary = prompt('Введите зарплату за месяц');
   
-      if (getSalary === '' || getSalary === null) {
-        break;
-      } 
-      numbers.push(+getSalary);
-    }
+//       if (getSalary === '' || getSalary === null) {
+//         break;
+//       } 
+//       numbers.push(+getSalary);
+//     }
   
-    let sum = 0;
-    for (let num of numbers) {
-      sum += num / numbers.length;
-    }
-    return sum;
-}
+//     let sum = 0;
+//     for (let num of numbers) {
+//       sum += num / numbers.length;
+//     }
+//     return sum;
+// }
   
-alert('Средняя зарплата ' + sumSalary() );
+// alert('Средняя зарплата ' + sumSalary() );
 
 
 
